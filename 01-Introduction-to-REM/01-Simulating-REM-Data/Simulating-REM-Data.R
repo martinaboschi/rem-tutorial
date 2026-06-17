ce5febf03c007cf61fbc8cfe362318313fa40524
martinaboschi <123976778+martinaboschi@users.noreply.github.com>
2026-06-17 15:04:21 +0200

    update

---
+++ 01-Introduction-to-REM/01-Simulating-REM-Data/Simulating-REM-Data.R
@@ -0,0 +1,8 @@
+## ----setup, include=FALSE-----------------------------------------------------
+knitr::knit_hooks$set(purl = knitr::hook_purl)
+knitr::opts_chunk$set(echo = TRUE)
+
+## ----message=FALSE, warning=FALSE---------------------------------------------
+# remotes::install_github("franciscorichter/amore")
+library(amore)
+
---
+++ 01-Introduction-to-REM/01-Simulating-REM-Data/Simulating-REM-Data.md
@@ -0,0 +1,12 @@
+# Application to Alien Species Invasions
+
+## Setup
+
+``` r
+# remotes::install_github("franciscorichter/amore")
+library(amore)
+```
+
+/Users/martina.boschi/Dropbox/Martina/PhD/12-REM-tutorial/rem-tutorial/01-Introduction-to-REM/01-Simulating-REM-Data/Simulating-REM-Data.R
+
+## 1.
